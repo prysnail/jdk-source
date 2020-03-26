e7e532b34a5d0e87ac30a3facad3b7d20b7ca4aa
prysnail <prysnail@outlook.com>
2020-03-26 21:18:12 +0800

    LinkedHashMap 源码注释

--- src/main/java/java/util/LinkedHashMap.java
+++ src/main/java/java/util/LinkedHashMap.java
@@ -106,9 +106,7 @@ public class LinkedHashMap<K,V>
     }
 
     /**
-     * Called by superclass constructors and pseudoconstructors (clone,
-     * readObject) before any entries are inserted into the map.  Initializes
-     * the chain.
+     * HashMap中的子类初始化钩子
      */
     @Override
     void init() {
@@ -117,9 +115,8 @@ public class LinkedHashMap<K,V>
     }
 
     /**
-     * Transfers all entries to new table array.  This method is called
-     * by superclass resize.  It is overridden for performance, as it is
-     * faster to iterate using our linked list.
+     * 因为性能问题而覆盖
+     * 遍历双向链表，具有更高的性能
      */
     @Override
     void transfer(HashMap.Entry[] newTable, boolean rehash) {
@@ -135,12 +132,7 @@ public class LinkedHashMap<K,V>
 
 
     /**
-     * Returns <tt>true</tt> if this map maps one or more keys to the
-     * specified value.
-     *
-     * @param value value whose presence in this map is to be tested
-     * @return <tt>true</tt> if this map maps one or more keys to the
-     *         specified value
+     * 遍历双链表来查找value
      */
     public boolean containsValue(Object value) {
         // Overridden to take advantage of faster iterator
@@ -156,32 +148,18 @@ public class LinkedHashMap<K,V>
         return false;
     }
 
-    /**
-     * Returns the value to which the specified key is mapped,
-     * or {@code null} if this map contains no mapping for the key.
-     *
-     * <p>More formally, if this map contains a mapping from a key
-     * {@code k} to a value {@code v} such that {@code (key==null ? k==null :
-     * key.equals(k))}, then this method returns {@code v}; otherwise
-     * it returns {@code null}.  (There can be at most one such mapping.)
-     *
-     * <p>A return value of {@code null} does not <i>necessarily</i>
-     * indicate that the map contains no mapping for the key; it's also
-     * possible that the map explicitly maps the key to {@code null}.
-     * The {@link #containsKey containsKey} operation may be used to
-     * distinguish these two cases.
-     */
+
     public V get(Object key) {
         Entry<K,V> e = (Entry<K,V>)getEntry(key);
         if (e == null)
             return null;
+        //记录访问顺序
         e.recordAccess(this);
         return e.value;
     }
 
     /**
-     * Removes all of the mappings from this map.
-     * The map will be empty after this call returns.
+     * 删除所有元素
      */
     public void clear() {
         super.clear();
@@ -192,7 +170,7 @@ public class LinkedHashMap<K,V>
      * LinkedHashMap entry.
      */
     private static class Entry<K,V> extends HashMap.Entry<K,V> {
-        // These fields comprise the doubly linked list used for iteration.
+        //双向链表
         Entry<K,V> before, after;
 
         Entry(int hash, K key, V value, HashMap.Entry<K,V> next) {
@@ -215,10 +193,9 @@ public class LinkedHashMap<K,V>
         }
 
         /**
-         * This method is invoked by the superclass whenever the value
-         * of a pre-existing entry is read by Map.get or modified by Map.set.
-         * If the enclosing Map is access-ordered, it moves the entry
-         * to the end of the list; otherwise, it does nothing.
+         * Map.get、Map.set调用此方法
+         * 如果按访问顺序（即：accessOrder为true),则将元素移动到list尾部
+         * 按插入顺序，则不做任何
          */
         void recordAccess(HashMap<K,V> m) {
             LinkedHashMap<K,V> lm = (LinkedHashMap<K,V>)m;
@@ -290,9 +267,7 @@ public class LinkedHashMap<K,V>
     Iterator<Map.Entry<K,V>> newEntryIterator() { return new EntryIterator(); }
 
     /**
-     * This override alters behavior of superclass put method. It causes newly
-     * allocated entry to get inserted at the end of the linked list and
-     * removes the eldest entry if appropriate.
+     * 覆盖addEntry，在此基础上删除最久的元素（如果允许）
      */
     void addEntry(int hash, K key, V value, int bucketIndex) {
         super.addEntry(hash, key, value, bucketIndex);
@@ -305,28 +280,21 @@ public class LinkedHashMap<K,V>
     }
 
     /**
-     * This override differs from addEntry in that it doesn't resize the
-     * table or remove the eldest entry.
+     * 覆盖crateEntry，添加将新建元素维护到双向链表中
      */
     void createEntry(int hash, K key, V value, int bucketIndex) {
         HashMap.Entry<K,V> old = table[bucketIndex];
         Entry<K,V> e = new Entry<>(hash, key, value, old);
         table[bucketIndex] = e;
+        //新建元素维护到双向链表中
         e.addBefore(header);
         size++;
     }
 
     /**
-     * Returns <tt>true</tt> if this map should remove its eldest entry.
-     * This method is invoked by <tt>put</tt> and <tt>putAll</tt> after
-     * inserting a new entry into the map.  It provides the implementor
-     * with the opportunity to remove the eldest entry each time a new one
-     * is added.  This is useful if the map represents a cache: it allows
-     * the map to reduce memory consumption by deleting stale entries.
      *
-     * <p>Sample use: this override will allow the map to grow up to 100
-     * entries and then delete the eldest entry each time a new entry is
-     * added, maintaining a steady state of 100 entries.
+     * true：删除最久的元素，被 put/putAll方法调用。当用于缓存时，非常有用：删除旧元素控制内存消耗
+     * 如：
      * <pre>
      *     private static final int MAX_ENTRIES = 100;
      *
@@ -334,28 +302,6 @@ public class LinkedHashMap<K,V>
      *        return size() > MAX_ENTRIES;
      *     }
      * </pre>
-     *
-     * <p>This method typically does not modify the map in any way,
-     * instead allowing the map to modify itself as directed by its
-     * return value.  It <i>is</i> permitted for this method to modify
-     * the map directly, but if it does so, it <i>must</i> return
-     * <tt>false</tt> (indicating that the map should not attempt any
-     * further modification).  The effects of returning <tt>true</tt>
-     * after modifying the map from within this method are unspecified.
-     *
-     * <p>This implementation merely returns <tt>false</tt> (so that this
-     * map acts like a normal map - the eldest element is never removed).
-     *
-     * @param    eldest The least recently inserted entry in the map, or if
-     *           this is an access-ordered map, the least recently accessed
-     *           entry.  This is the entry that will be removed it this
-     *           method returns <tt>true</tt>.  If the map was empty prior
-     *           to the <tt>put</tt> or <tt>putAll</tt> invocation resulting
-     *           in this invocation, this will be the entry that was just
-     *           inserted; in other words, if the map contains a single
-     *           entry, the eldest entry is also the newest.
-     * @return   <tt>true</tt> if the eldest entry should be removed
-     *           from the map; <tt>false</tt> if it should be retained.
      */
     protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
         return false;
