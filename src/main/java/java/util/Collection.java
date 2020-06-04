da9fecf49834faa96cc926984b51e9012364c85c
prysnail <prysnail@outlook.com>
2020-06-04 17:18:58 +0800

    Collection部分注释

---
+++ src/main/java/java/util/Collection.java
@@ -0,0 +1,238 @@
+/*
+ * Copyright (c) 1997, 2010, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+package java.util;
+
+/**
+ *
+ * 集合层次中的根接口，
+ * This interface is typically used to pass collections around and manipulate them where
+ * maximum generality is desired.
+ *
+ * 实现该接口的类应当提供两个标准构造器 ：
+ * 1. 无参构造器
+ * 2. 以Collection为参的构造器
+ *
+ * 添加非法元素时，根据具体实现，成功或
+ * 抛出unchecked exception，typically <tt>NullPointerException</tt> or <tt>ClassCastException</tt>.
+ *
+ * 每个集合决定自己的同步策略
+ *
+ * @author  Josh Bloch
+ * @author  Neal Gafter
+ * @see     Set
+ * @see     List
+ * @see     Map
+ * @see     SortedSet
+ * @see     SortedMap
+ * @see     HashSet
+ * @see     TreeSet
+ * @see     ArrayList
+ * @see     LinkedList
+ * @see     Vector
+ * @see     Collections
+ * @see     Arrays
+ * @see     AbstractCollection
+ * @since 1.2
+ */
+
+public interface Collection<E> extends Iterable<E> {
+    // Query Operations
+
+    /**
+     * more than <tt>Integer.MAX_VALUE</tt> elements, returns <tt>Integer.MAX_VALUE</tt>.
+     *
+     */
+    int size();
+
+
+    boolean isEmpty();
+
+    /**
+     * @throws ClassCastException
+     * @throws NullPointerException
+     */
+    boolean contains(Object o);
+
+    /**
+     * 不保证返回的顺序
+     */
+    Iterator<E> iterator();
+
+    /**
+     * 如果迭代器保证顺序，则该方法必须以相同顺序返回元素
+     *
+     * 返回的数组是安全的，该集合不维护该数组的引用
+     *
+     * 该方法是数组和集合的桥梁
+     *
+     */
+    Object[] toArray();
+
+    /**
+     * Returns an array containing all of the elements in this collection;
+     * the runtime type of the returned array is that of the specified array.
+     * If the collection fits in the specified array, it is returned therein.
+     * Otherwise, a new array is allocated with the runtime type of the
+     * specified array and the size of this collection.
+     *
+     * <p>If this collection fits in the specified array with room to spare
+     * (i.e., the array has more elements than this collection), the element
+     * in the array immediately following the end of the collection is set to
+     * <tt>null</tt>.  (This is useful in determining the length of this
+     * collection <i>only</i> if the caller knows that this collection does
+     * not contain any <tt>null</tt> elements.)
+     *
+     * <p>If this collection makes any guarantees as to what order its elements
+     * are returned by its iterator, this method must return the elements in
+     * the same order.
+     *
+     * <p>Like the {@link #toArray()} method, this method acts as bridge between
+     * array-based and collection-based APIs.  Further, this method allows
+     * precise control over the runtime type of the output array, and may,
+     * under certain circumstances, be used to save allocation costs.
+     *
+     * <p>Suppose <tt>x</tt> is a collection known to contain only strings.
+     * The following code can be used to dump the collection into a newly
+     * allocated array of <tt>String</tt>:
+     *
+     * <pre>
+     *     String[] y = x.toArray(new String[0]);</pre>
+     *
+     * Note that <tt>toArray(new Object[0])</tt> is identical in function to
+     * <tt>toArray()</tt>.
+     *
+     * @param a the array into which the elements of this collection are to be
+     *        stored, if it is big enough; otherwise, a new array of the same
+     *        runtime type is allocated for this purpose.
+     * @return an array containing all of the elements in this collection
+     * @throws ArrayStoreException if the runtime type of the specified array
+     *         is not a supertype of the runtime type of every element in
+     *         this collection
+     * @throws NullPointerException if the specified array is null
+     */
+    <T> T[] toArray(T[] a);
+
+    // Modification Operations
+
+    /**
+     *
+     * <tt>true</tt>：集合改变
+     * <tt>false</tt>：已经存在并且不允许重复
+     *
+     * 如果不是因已经存在元素而拒绝添加元素，必须抛出异常
+     *
+     * @throws UnsupportedOperationException
+     * @throws ClassCastException
+     * @throws NullPointerException
+     * @throws IllegalArgumentException if some property of the element
+     *         prevents it from being added to this collection
+     * @throws IllegalStateException if the element cannot be added at this
+     *         time due to insertion restrictions
+     */
+    boolean add(E e);
+
+    /**
+     * <tt>true</tt>：集合改变
+     *
+     * @throws ClassCastException
+     * @throws NullPointerException
+     * @throws UnsupportedOperationException
+     */
+    boolean remove(Object o);
+
+
+    // Bulk Operations
+
+
+    boolean containsAll(Collection<?> c);
+
+
+    boolean addAll(Collection<? extends E> c);
+
+
+    boolean removeAll(Collection<?> c);
+
+
+    boolean retainAll(Collection<?> c);
+
+
+    void clear();
+
+
+    // Comparison and hashing
+
+    /**
+     * Compares the specified object with this collection for equality. <p>
+     *
+     * While the <tt>Collection</tt> interface adds no stipulations to the
+     * general contract for the <tt>Object.equals</tt>, programmers who
+     * implement the <tt>Collection</tt> interface "directly" (in other words,
+     * create a class that is a <tt>Collection</tt> but is not a <tt>Set</tt>
+     * or a <tt>List</tt>) must exercise care if they choose to override the
+     * <tt>Object.equals</tt>.  It is not necessary to do so, and the simplest
+     * course of action is to rely on <tt>Object</tt>'s implementation, but
+     * the implementor may wish to implement a "value comparison" in place of
+     * the default "reference comparison."  (The <tt>List</tt> and
+     * <tt>Set</tt> interfaces mandate such value comparisons.)<p>
+     *
+     * The general contract for the <tt>Object.equals</tt> method states that
+     * equals must be symmetric (in other words, <tt>a.equals(b)</tt> if and
+     * only if <tt>b.equals(a)</tt>).  The contracts for <tt>List.equals</tt>
+     * and <tt>Set.equals</tt> state that lists are only equal to other lists,
+     * and sets to other sets.  Thus, a custom <tt>equals</tt> method for a
+     * collection class that implements neither the <tt>List</tt> nor
+     * <tt>Set</tt> interface must return <tt>false</tt> when this collection
+     * is compared to any list or set.  (By the same logic, it is not possible
+     * to write a class that correctly implements both the <tt>Set</tt> and
+     * <tt>List</tt> interfaces.)
+     *
+     * @param o object to be compared for equality with this collection
+     * @return <tt>true</tt> if the specified object is equal to this
+     * collection
+     *
+     * @see Object#equals(Object)
+     * @see Set#equals(Object)
+     * @see List#equals(Object)
+     */
+    boolean equals(Object o);
+
+    /**
+     * Returns the hash code value for this collection.  While the
+     * <tt>Collection</tt> interface adds no stipulations to the general
+     * contract for the <tt>Object.hashCode</tt> method, programmers should
+     * take note that any class that overrides the <tt>Object.equals</tt>
+     * method must also override the <tt>Object.hashCode</tt> method in order
+     * to satisfy the general contract for the <tt>Object.hashCode</tt> method.
+     * In particular, <tt>c1.equals(c2)</tt> implies that
+     * <tt>c1.hashCode()==c2.hashCode()</tt>.
+     *
+     * @return the hash code value for this collection
+     *
+     * @see Object#hashCode()
+     * @see Object#equals(Object)
+     */
+    int hashCode();
+}
